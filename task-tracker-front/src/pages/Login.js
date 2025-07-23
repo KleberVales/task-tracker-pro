f0fe566ddd80c65017356ad236c1036ad7a9eaba
Kleber Vales <klebervales.dev@gmail.com>
2025-07-23 18:44:04 -0300

    Update the front end

--- task-tracker-front/src/pages/Login.js
+++ task-tracker-front/src/pages/Login.js
@@ -1,3 +1,15 @@
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-const navigate = useNavigate();
-navigate('/dashboard');
+
+function Login() {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h2>Login</h2>
+      {/* restante da lógica */}
+    </div>
+  );
+}
+
+export default Login;
