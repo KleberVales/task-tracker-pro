d3dbb0fcc5ff325b1b87a4b9405252aac68a8485
Kleber Vales <klebervales.dev@gmail.com>
2025-07-28 09:19:50 -0300

    feat: Update the microservice

--- notification-service/src/main/java/com/tasktracker/notification/dto/EmailNotificationRequest.java
+++
@@ -1,9 +0,0 @@
-package com.tasktracker.notification.dto;
-
-public class EmailNotificationRequest {
-    private String to;
-    private String subject;
-    private String body;
-
-    // Getters e Setters
-}
